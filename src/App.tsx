e68e3ac0cef6eba7a57c0582398d443390365be7
stevennguyen003 <96093087+stevennguyen003@users.noreply.github.com>
2024-03-27 19:47:07 -0400

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Routes, Route, Navigate } from "react-router";
 import LandingPage from './Final Project/LandingPage';
 
 const CLIENT_ID = "9cecbacf65564b4c80471ac14f86d430";
-const CLIENT_SECRET = "ac38790dd2b14fbeac37d829f8be267a";
+const SECRET_ID = "ac38790dd2b14fbeac37d829f8be267a";
 
 function App() {
   return (
