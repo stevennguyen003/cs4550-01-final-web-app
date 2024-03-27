3dff8f4e5005431c7aa1b9abe70a8c19e492edbd
stevennguyen003 <96093087+stevennguyen003@users.noreply.github.com>
2024-03-27 19:48:46 -0400

    remove id

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,6 @@ import { HashRouter } from "react-router-dom";
 import { Routes, Route, Navigate } from "react-router";
 import LandingPage from './Final Project/LandingPage';
 
-const CLIENT_ID = "9cecbacf65564b4c80471ac14f86d430";
-const SECRET_ID = "ac38790dd2b14fbeac37d829f8be267a";
-
 function App() {
   return (
     <HashRouter>
