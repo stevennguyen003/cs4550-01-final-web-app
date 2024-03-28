025ed50bce7ce7e346f3d196f17bca2cefb9ed38
stevennguyen003 <96093087+stevennguyen003@users.noreply.github.com>
2024-03-28 01:56:47 -0400

    home page

--- src/Final Project/Home/index.tsx
+++ src/Final Project/Home/index.tsx
@@ -1,7 +1,22 @@
+import "./index.css";
 function Home() {
     return (
         <>
-            <h1>Home Screen</h1>
+            <div className="home-page-container wd-flex-row-container">
+                <div className="home-page-settings-container">
+                    <div className="home-page-settings-pill">
+                        <h1>Settings</h1>
+                    </div>
+                </div>
+                <div className="home-page-body-container">
+                    <div className="home-page-body-pill">
+                        <h1>Body</h1>
+                    </div>
+                </div>
+            </div>
+            <div className="playback-bar-container">
+                <h1>Playback</h1>
+            </div>
         </>
     )
 }
