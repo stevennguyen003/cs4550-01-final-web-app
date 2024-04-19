be93c71291e55577928219dd88ef862fa1d8c86b
stevennguyen003 <96093087+stevennguyen003@users.noreply.github.com>
2024-04-19 04:52:08 -0400

    little more styling, changed input field for profile picture to file

--- src/Final Project/CreateAccount/index.tsx
+++ src/Final Project/CreateAccount/index.tsx
@@ -8,23 +8,23 @@ function CreateAccount() {
                     <h1 className="create-account-header">create an account &#127769;</h1> <br />
                     <form>
                         <div className="form-group">
-                            <label htmlFor="login-account-email"><b>EMAIL*</b></label> <br />
+                            <label htmlFor="login-account-email"><b>EMAIL <span className="asterick">*</span></b></label> <br />
                             <input type="email" className="form-control" id="login-account-email" />
                         </div>
                         <div className="form-group">
-                            <label htmlFor="login-account-username"><b>USERNAME*</b></label> <br />
+                            <label htmlFor="login-account-username"><b>USERNAME <span className="asterick">*</span></b></label> <br />
                             <input type="username" className="form-control" id="login-account-username" />
                         </div>
                         <div className="form-group">
-                            <label htmlFor="login-account-password"><b>PASSWORD*</b></label> <br />
+                            <label htmlFor="login-account-password"><b>PASSWORD <span className="asterick">*</span></b></label> <br />
                             <input type="password" className="form-control" id="login-account-password" />
                         </div>
                         <div className="form-group">
-                            <label htmlFor="login-account-password"><b>RE-ENTER PASSWORD*</b></label> <br />
+                            <label htmlFor="login-account-password"><b>RE-ENTER PASSWORD <span className="asterick">*</span></b></label> <br />
                             <input type="password" className="form-control" id="login-account-password" />
                         </div>
                         <div className="form-group">
-                            <label htmlFor="login-account-dob"><b>DATE OF BIRTH*</b></label>
+                            <label htmlFor="login-account-dob"><b>DATE OF BIRTH <span className="asterick">*</span></b></label>
                             <input type="date" className="form-control form-control-sm" id="login-account-dob"/>
                         </div>
                         <br />
--- src/Final Project/CreateProfile/index.tsx
+++ src/Final Project/CreateProfile/index.tsx
@@ -9,29 +9,29 @@ function CreateProfile() {
                     <h3 className="create-account-header">let's create a profile for you!</h3><br />
                     <form>
                         <div className="form-group">
-                            <label htmlFor="login-account-first-name"><b>FIRST NAME*</b></label> <br />
+                            <label htmlFor="login-account-first-name"><b>FIRST NAME <span className="asterick">*</span></b></label> <br />
                             <input type="text" className="form-control" id="login-account-first-name" />
                         </div>
                         <div className="form-group">
-                            <label htmlFor="login-account-last-name"><b>LAST NAME*</b></label> <br />
+                            <label htmlFor="login-account-last-name"><b>LAST NAME <span className="asterick">*</span></b></label> <br />
                             <input type="text" className="form-control" id="login-account-last-name" />
                         </div>
                         <div className="form-group">
-                            <label htmlFor="login-account-display-name"><b>DISPLAY NAME*</b></label> <br />
+                            <label htmlFor="login-account-display-name"><b>DISPLAY NAME <span className="asterick">*</span></b></label> <br />
                             <input type="text" className="form-control" id="login-account-display-name" />
                         </div>
                         <div className="form-group">
-                            <label htmlFor="login-account-picture"><b>PROFILE PICTURE*</b></label> <br />
-                            <input type="image" className="form-control" id="login-account-picture" alt="Profile Picture" />
+                            <label htmlFor="login-account-description"><b>BIO</b></label> <br />
+                            <input type="text" className="form-control" id="login-account-description" />
                         </div>
                         <div className="form-group">
-                            <label htmlFor="login-account-description"><b>PROFILE DESCRIPTION*</b></label> <br />
-                            <input type="text" className="form-control" id="login-account-description" />
-                        </div>  
-                        <div className="form-group">
-                            <label htmlFor="login-account-level"><b>YEARS OF EXPERIENCE IN THE GYM*</b></label> <br />
+                            <label htmlFor="login-account-level"><b>YEARS OF EXPERIENCE IN THE GYM <span className="asterick">*</span></b></label> <br />
                             <input type="number" className="form-control" id="login-account-level" />
-                        </div> 
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="login-account-picture"><b>PROFILE PICTURE <span className="asterick">*</span></b></label> <br />
+                            <input type="file" className="form-control" id="login-account-picture" />
+                        </div>
                         <br />
                         <Link to="/Home" className="btn btn-primary"><b>CREATE PROFILE</b></Link>
                         <Link to="/">Already have an account?</Link>
