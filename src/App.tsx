f77cbbd2d9b4050d789d48e0fb163d8be97bf134
stevennguyen003 <96093087+stevennguyen003@users.noreply.github.com>
2024-04-21 04:08:50 -0400

    friend list and api to front end

--- src/App.tsx
+++ src/App.tsx
@@ -9,54 +9,16 @@ import LoadingScreen from "./Final Project/Utilities/LoadingScreen";
 import CreateProfile from "./Final Project/Profile/CreateProfile";
 
 function App() {
-  /*
-  //const [searchInput, setSearchInput] = useState("");
-  const [accessToken, setAccessToken] = useState("");
-
-  const searchInput = "Taylor Swift";
-
-  useEffect(() => {
-    var authParameters = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      body: 'grant_type=client_credentials&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET
-    }
-    fetch('https://accounts.spotify.com/api/token', authParameters)
-      .then(result => result.json())
-      .then(data => setAccessToken(data.access_token))
-
-    // Input Error Handling Here
-  }, [])
-
-  async function search() {
-    console.log("Search for " + searchInput);
-
-    // Get request using search to get the Artist ID
-    var artistParameters = {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + accessToken
-      }
-    }
-    var artistID = await fetch('http://api.spotify.com/v1/search?q=' + searchInput + '&type=artist', artistParameters)
-      .then(response => response.json())
-      .then(data => console.log(data))
-    // Get request with Artist ID grab all the albums from that artist
-  }
-  */
 
   return (
     <HashRouter>
       <Suspense fallback={<LoadingScreen />}>
         <Routes>
-        <Route path="/*" element={<Navigate to="/Main/Login" />} />
+        <Route path="/" element={<Navigate to="/Main/Login" />} />
           <Route path="Main/:param" element={<LandingPage />} />
           <Route path="Profile/create" element={<CreateProfile />} />
-          <Route path="Home" element={<Home />} />
-
+          <Route path="Home/*" element={<Home />} />
+          <Route path="Profile/:UserId/*" element={<h1>PROFILE</h1>} />
         </Routes>
       </Suspense>
     </HashRouter>
