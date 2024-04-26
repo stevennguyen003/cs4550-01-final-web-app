79beb15cf4cb3fa23f7bdbfcdd402881b3c66340
Steven <stevennguyen003@gmail.com>
2024-04-26 16:01:01 -0400

    replace local host w env

--- src/Final Project/Home/Chat/client.tsx
+++ src/Final Project/Home/Chat/client.tsx
@@ -2,14 +2,12 @@ import axios from "axios";
 axios.defaults.withCredentials = true;
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 export const getConversation = async () => {
-    const response = await axios.get("http://localhost:4000/api/openai/conversation" ||
-        `${BACKEND_URL}/api/openai/conversation`);
+    const response = await axios.get(`${BACKEND_URL}/api/openai/conversation`);
     return response.data;
 };
 
 export const postMessage = async (message: any) => {
-    const response = await axios.post(
-        "http://localhost:4000/api/openai/conversation" || `${BACKEND_URL}/api/openai/conversation`,
+    const response = await axios.post(`${BACKEND_URL}/api/openai/conversation`,
         message);
     return response.data;
 };
